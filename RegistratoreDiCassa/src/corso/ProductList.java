39aa97ab16a1c8daaa1713a7591d3df2ab8c6b8a
Utente <Utente@DESKTOP-J6D9I9R>
2019-09-05 15:17:54 +0200

    Ho spostato il main in ProductListDemo

--- RegistratoreDiCassa/src/corso/ProductList.java
+++ RegistratoreDiCassa/src/corso/ProductList.java
@@ -15,14 +15,5 @@ public class ProductList extends JPanel{
 		JList<String> lista = new JList<String>(prodotto);
 		
 		add(lista);
-		
-	}
-	
-	public static void main(String args[]) {
-		JFrame frame = new JFrame("Lista dei prodotti");
-		frame.add(new ProductList());
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
 	}
 }
