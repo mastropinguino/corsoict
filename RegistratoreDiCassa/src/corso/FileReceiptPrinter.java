2e1e913f44b5c4d3fe8fa5a48626ab5009205a4a
Utente <Utente@DESKTOP-J6D9I9R>
2019-09-16 12:15:22 +0200

    Prima implementazione file ReceiptPrinter

---
+++ RegistratoreDiCassa/src/corso/FileReceiptPrinter.java
@@ -0,0 +1,28 @@
+package corso;
+
+import java.io.*;
+
+
+public class FileReceiptPrinter {
+	File file;
+	
+	public FileReceiptPrinter(File file){
+		this.file = file;
+	}
+
+	public void scriviFile() {
+
+		try {
+			
+			FileWriter fw = new FileWriter(file);
+			fw.write("ID SCONTRINO\t");
+			fw.write("   DATA/ORA\t  ");
+			fw.write("TOTALE\t");
+			fw.write("IVA\t");
+			fw.flush();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+}
+}
