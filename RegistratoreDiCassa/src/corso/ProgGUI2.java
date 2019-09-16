467ce78c62356197369d274f719b4c96fa5e24f9
Lorenzo Cioni <Lorenzo Cioni@DESKTOP-J6D9I9R>
2019-09-16 11:05:15 +0200

    Ho riorganizzato la disposizione dei vari widgets:
    aggiunti insets per creare spazi, risistemata la grigiliain modo da
    allineare i vari panel, ridimensionati alcuni i panel. Aggiunto il
    titolo.

--- RegistratoreDiCassa/src/corso/ProgGUI2.java
+++ RegistratoreDiCassa/src/corso/ProgGUI2.java
@@ -5,91 +5,104 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-public class ProgGUI2 extends JFrame{
-	
-	
-	
+@SuppressWarnings("serial")
+public class ProgGUI2 extends JFrame {
+
 	JTextField totale;
-	
+
 	public ProgGUI2() {
+		
+		super("Registratore di cassa");
+		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		GridBagLayout gbLayout = new GridBagLayout();
 		GridBagConstraints c = new GridBagConstraints();
 		this.setLayout(gbLayout);
-		
-		//TextField totale
-		
+
+		// TextField totale
+
 		totale = new JTextField();
-		
+
 		c.gridx = 0;
 		c.gridy = 0;
-		c.gridwidth = 1;
+		c.gridwidth = 2;
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(10, 10, 10, 10);
+
 		
 		totale.setHorizontalAlignment(JLabel.CENTER);
-		//totale.setVerticalAlignment(JLabel.CENTER);
+		totale.setEnabled(false);
 		totale.setPreferredSize(new Dimension(250, 50));
 		add(totale, c);
-		
-		//Product selector
+
+		// Product selector
 		c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 1;
-		c.gridwidth = 1;
+		c.gridwidth = 2;
 		c.weightx = 0.5;
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(10, 10, 10, 10);
+
 		
 		ProductSelecter lorenzo = new ProductSelecter();
 		lorenzo.setPreferredSize(new Dimension(250, 90));
 		add(lorenzo, c);
-		
-		//Keypad
+
+		// Keypad
 		c = new GridBagConstraints();
 		c.gridx = 0;
-		c.gridy = 3;
+		c.gridy = 2;
 		c.gridwidth = 1;
 		c.weightx = 1;
-		
-		KeyPad paolo=new KeyPad(); 
-		
-		paolo.setPreferredSize(new Dimension(250, 400));
+		c.insets = new Insets(10, 10, 10, 10);
+
+		KeyPad paolo = new KeyPad();
+
+		paolo.setPreferredSize(new Dimension(200, 250));
 		add(paolo, c);
-		
-		//Lista prodotti
+
+		// Lista prodotti
 		c = new GridBagConstraints();
-		c.gridx = 2;
+		c.gridx = 3;
 		c.gridy = 0;
 		c.gridheight = 3;
 		c.fill = GridBagConstraints.BOTH;
 		c.weightx = 2;
 		c.weighty = 1;
-		
+		c.insets = new Insets(10, 10, 10, 10);
+
 		ProductList fatou = new ProductList();
-		fatou.setSize(new Dimension(150,700));
+		fatou.setPreferredSize(new Dimension(150, 400));
 		add(fatou, c);
 
-		//Bottone add product
+		// Bottone scontrino
 		c = new GridBagConstraints();
 		JButton scontrino = new JButton("Scontrino");
 		c.fill = GridBagConstraints.NONE;
 		c.gridx = 1;
-		c.gridy = 3;
+		c.gridy = 2;
 		c.gridwidth = 1;
 		c.anchor = GridBagConstraints.CENTER;
-		c.insets = new Insets(20, 20, 20, 20);
-		
-		scontrino.setPreferredSize(new Dimension(80, 60));
+		c.insets = new Insets(10, 10, 10, 10);
+
+		scontrino.setPreferredSize(new Dimension(100, 150));
 		add(scontrino, c);
-		
+
+		this.setLocationRelativeTo(null);
 		pack();
 		setVisible(true);
-		
+
 	}
-	
 
 }
