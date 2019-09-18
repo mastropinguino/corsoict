81d73668b2e9237f965fade7bf5969ebd9d53256
Lorenzo Cioni <Lorenzo Cioni@DESKTOP-J6D9I9R>
2019-09-18 15:39:53 +0200

    Aggiunto invokeLater

--- RegistratoreDiCassa/src/corso/MainProgGUI2.java
+++ RegistratoreDiCassa/src/corso/MainProgGUI2.java
@@ -1,14 +1,18 @@
 package corso;
 
+import javax.swing.SwingUtilities;
+
 public class MainProgGUI2 {
 
 	public static void main(String[] args) {
-
-		Settings.loadSettings("src/test/TestConfig.properties");
-		ProgGUI2 gui = new ProgGUI2();
-		gui.init();
-		gui.run();
-
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run(){
+				Settings.loadSettings("src/test/TestConfig.properties");
+				ProgGUI2 gui = new ProgGUI2();
+				gui.init();
+				gui.run();				
+			}
+		});
 	}
-
 }
