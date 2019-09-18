74503b1d443030a2ee0e685476b5cc6f6ac1e184
Utente <Utente@DESKTOP-J6D9I9R>
2019-09-18 12:24:20 +0200

    Aggiornata data e ora di sistema su scontrino

--- RegistratoreDiCassa/src/corso/FileReceiptPrinter.java
+++ RegistratoreDiCassa/src/corso/FileReceiptPrinter.java
@@ -1,28 +1,45 @@
 package corso;
 
 import java.io.*;
+import java.time.Instant;
+
+
+
+
+
 
 
 public class FileReceiptPrinter {
 	File file;
-	
+
 	public FileReceiptPrinter(File file){
 		this.file = file;
+		
 	}
+	
+	
+	
+	
 
 	public void scriviFile() {
 
 		try {
 			
 			FileWriter fw = new FileWriter(file);
-			fw.write("ID SCONTRINO\t");
-			fw.write("   DATA/ORA\t  ");
-			fw.write("TOTALE \t");
-			fw.write("IVA \t");
+			fw.write("1\n");
+			fw.write("\n");
+			Instant adesso = Instant.now();
+			String outputData = adesso.toString();
+			fw.write(outputData);
+			fw.write("prodotto1\n");
+			fw.write("2x\n");
+			fw.write("20,50€\n");
+			fw.write("4,51€\n ");
 			fw.flush();
 			fw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-}
+	}
+	
 }
